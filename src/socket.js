01b7ef5fcf72dd6710342eb72aa4993664b8572a
maryam <mariem.kerrous@gmail.com>
2026-06-02 22:12:38 +0100

    feat: emit join_user on socket connect

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,18 @@
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:5000", {
-  autoConnect: false, 
+  autoConnect: false,
+});
+
+socket.on("connect", () => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    const tokenPayload = JSON.parse(atob(token.split('.')[1]));
+    const userId = tokenPayload?.id || tokenPayload?._id || tokenPayload?.userId;
+    if (userId) {
+      socket.emit("join_user", userId);
+    }
+  }
 });
 
 export default socket;
