92b54d340124f80259da3db7e5a9bfe8f793468b
ass44ma <assma.boutezrout@gmail.com>
2026-06-03 17:17:18 +0100

    fix: password reset error

--- backend/models/User.js
+++ backend/models/User.js
@@ -42,9 +42,9 @@ const UserSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Encrypt password using bcrypt before saving
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    next();
+    return;
   }
 
   const salt = await bcrypt.genSalt(10);
